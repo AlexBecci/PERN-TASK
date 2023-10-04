e9a27419c819dc9a518ec372fcbd530e1db8d9eb
Alex <beccialex2002@gmail.com>
2023-10-04 18:47:06 +0200

    Create tasks to postgreSQL

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,11 +1,27 @@
+import { pool } from "../db.js";
+
 export const getAllTasks = (req, res) => {
   res.send("Obteniendo tareas");
 };
 export const getTaks = (req, res) => {
   res.send("Obteniendo tarea");
 };
-export const createTaks = (req, res) => {
-  res.send("creando tarea");
+export const createTaks = async (req, res, next) => {
+  const { title, description } = req.body;
+  //db insert
+
+  try {
+    const result = await pool.query(
+      "INSERT INTO task (title, description) VALUES ($1, $2) RETURNING *",
+      [title, description]
+    );
+    res.json(result.rows[0]);
+  } catch (error) {
+    if (error.code === "23505") {
+      return res.send("Task already exist");
+    }
+    next(error);
+  }
 };
 export const updateTaks = (req, res) => {
   res.send("ACtualizando tarea");
