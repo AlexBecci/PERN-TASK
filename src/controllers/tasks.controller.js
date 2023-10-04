49b467eaf360e5a9bbad318e318359a5a5936952
Alex <beccialex2002@gmail.com>
2023-10-04 18:59:52 +0200

    Get Tasks

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,15 +1,16 @@
 import { pool } from "../db.js";
 
-export const getAllTasks = (req, res) => {
-  res.send("Obteniendo tareas");
+export const getAllTasks = async (req, res, next) => {
+  const result = await pool.query("SELECT * FROM task");
+  return res.json(result.rows);
 };
+
 export const getTaks = (req, res) => {
   res.send("Obteniendo tarea");
 };
 export const createTaks = async (req, res, next) => {
   const { title, description } = req.body;
   //db insert
-
   try {
     const result = await pool.query(
       "INSERT INTO task (title, description) VALUES ($1, $2) RETURNING *",
@@ -18,7 +19,9 @@ export const createTaks = async (req, res, next) => {
     res.json(result.rows[0]);
   } catch (error) {
     if (error.code === "23505") {
-      return res.send("Task already exist");
+      return res.status(409).json({
+        message: "Task already exist",
+      });
     }
     next(error);
   }
