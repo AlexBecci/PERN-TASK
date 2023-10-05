7acd2efba7c8acf592da494a256751095c4b3a4e
Alex <beccialex2002@gmail.com>
2023-10-05 02:20:40 +0200

    Register

---
+++ database/init.sql
@@ -0,0 +1,14 @@
+CREATE TABLE task(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) UNIQUE NOT NULL,
+    description TEXT
+);
+
+create TABLE users(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
+);
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,8 +1,29 @@
+import bcrypt from "bcrypt";
+import { pool } from "../db.js";
+
 export const signin = (req, res) => {
   res.send("Ingresando");
 };
-export const signup = (req, res) => {
-  res.send("registrando");
+
+export const signup = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log(hashedPassword);
+
+    const result = await pool.query(
+      "INSERT INTO users(name, email, password) VALUES($1, $2, $3) RETURNING *",
+      [name, email, hashedPassword]
+    );
+    console.log(result);
+    return res.json(result.rows[0]);
+  } catch (error) {
+    if (error.code === "23505") {
+      return res.status(400).json({
+        message: "Email already exist",
+      });
+    }
+  }
 };
 export const signout = (req, res) => {
   res.send("Cerrando sesion");
