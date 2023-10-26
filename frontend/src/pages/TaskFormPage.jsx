aeac9f5267cf68da09f3e15b756c540627685650
Alex <beccialex2002@gmail.com>
2023-10-26 15:36:21 +0200

    Task form page method post to database

--- frontend/src/pages/TaskFormPage.jsx
+++ frontend/src/pages/TaskFormPage.jsx
@@ -1,8 +1,47 @@
+import { Textarea, Input, Card, Label, Button } from "../components/ui";
+import { useForm } from "react-hook-form";
+import {useNavigate} from "react-router-dom"
+import { createTaskRequest } from "../api/tasks.api";
 
 function TaskFormPage() {
+  const { register, handleSubmit, formState: {errors} } = useForm();
+  const navigate = useNavigate(); 
+
+
+  const onSubmit = handleSubmit(async (data) => {
+    const res = await createTaskRequest(data);
+    navigate("/tasks")
+  });
+
   return (
-    <div>TaskFormPage</div>
-  )
+    <div className="flex h-[80vh] justify-center items-center">
+      <Card>
+        <h2 className="text-3xl font-bold">Crate Task</h2>
+        <form onSubmit={onSubmit}>
+          <Label htmlFor="title">Title</Label>
+          <Input
+            type="text"
+            placeholder="Title"
+            autoFocus
+            {...register("title", {
+              required: true,
+            })}
+          />
+          {errors.title && (
+            <span className="text-red-500"> Title is required</span>
+          )}
+          <Label htmlFor="description"> Description</Label>
+          <Textarea
+            placeholder="Description"
+            rows={3}
+            {...register("description")}
+          ></Textarea>
+
+          <Button>Create</Button>
+        </form>
+      </Card>
+    </div>
+  );
 }
 
-export default TaskFormPage
+export default TaskFormPage;
