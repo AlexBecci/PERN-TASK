6be2f86188dcbb43465cda400d7c8e7af06180e5
Alex <beccialex2002@gmail.com>
2023-10-26 21:20:49 +0200

    ready for deploy

--- src/app.js
+++ src/app.js
@@ -4,13 +4,14 @@ import taskRoutes from "./routes/tasks.routes.js";
 import authRoutes from "./routes/auth.routes.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { ORIGIN } from "./config.js";
 
 const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true
+    origin: ORIGIN,
+    credentials: true,
   })
 );
 app.use(morgan("dev"));
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import app from "./app.js";
+import {PORT} from "./config.js"
+app.listen(PORT);
 
-app.listen(3000);
-
-console.log("Server on port", 3000);
+console.log("Server on port", PORT);
